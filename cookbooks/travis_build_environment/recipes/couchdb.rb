7ca9399410be16eabf1a8a74f198f761f98b566f
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-07-31 13:11:11 +0200

    Update couchdb.rb

--- cookbooks/travis_build_environment/recipes/couchdb.rb
+++ cookbooks/travis_build_environment/recipes/couchdb.rb
@@ -13,6 +13,13 @@ when 'bionic'
     components ['main']
     key 'https://couchdb.apache.org/repo/bintray-pubkey.asc'
   end
+when 'focal'
+  apt_repository 'couchdb' do
+    uri 'https://apache.bintray.com/couchdb-deb'
+    distribution 'focal'
+    components ['main']
+    key 'https://couchdb.apache.org/repo/bintray-pubkey.asc'
+  end
 end
 
 package 'couchdb'
